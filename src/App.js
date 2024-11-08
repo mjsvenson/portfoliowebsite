cb92a0955817900bcdce33eea7b2b843f4a08851
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2024-11-08 00:08:10 -0500

    Getting frustrated learning react and making the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import BasicExample from "./Components/NavBar";
+
 
 function App() {
   return (
     <div className="App">
+      <BasicExample />
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,31 +1,23 @@
-import { NavBar, Container } from "react-bootstrap"; 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export const NavBar = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-        <Navbar.Brand href="#home">
-            <img src = {''} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" >  
-            <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Matthew Svenson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>          
+            <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
-            </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
+  );
 }
+
+export default BasicExample;
