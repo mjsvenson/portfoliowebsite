371257b9ba3e5e5c970e149371d7a8538b59def2
Svenson <msvenson@necc.mass.edu>
2025-05-02 10:38:08 -0400

    Added background for navbar, can't figure out how to get the image in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
+import Logo from "./logo.png";
 
 export default function Home() {
+  console.log(Logo);
+
   return (
     <div id="WebPage">
-    <p> OK I need to make a general layout for my portfolio....</p>
-    <div id="NavBar">
-      <p> this is where I would put the navbar</p>
+      <div id="NavBar" className="bg-black h-[100]">
+        <img src="./logo.png" />
+      <p className="text-white"> this is where I would put the navbar</p>
     </div>
+    <p> OK I need to make a general layout for my portfolio....</p>
     <div id="FrontPageIntro">
       <p>This is where I would put the intro page.</p>
     </div>
