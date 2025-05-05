91603d8e84ddf07a2ff3b5e66c0a79db92f5797e
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2025-05-04 22:14:30 -0400

    Added more to the navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -5,8 +5,12 @@ export default function NavBar() {
 
   return (
     <div id="NavBar" className="bg-black h-[100]">
-    <Image src={Logo} alt="Logo" width={100} height={100} />
-      <p className="text-white"> this is where I would put the navbar</p>
+    <Image src={Logo} alt="Logo" width={100} height={100} className='float-left'/>
+      <p className="text-white float-right my-[30] mx-[20]"> Certifications </p>
+      <p className="text-white float-right my-[30] mx-[20]"> Projects </p>
+      <p className="text-white float-right my-[30] mx-[20]"> Work Experience </p>
+      <p className="text-white float-right my-[30] mx-[20]"> Education </p>
+
     </div>
   )
 }
