c39dd16e957ac2b72b48d1ee063dab66104e4641
Svenson <msvenson@necc.mass.edu>
2025-05-08 09:42:45 -0400

    Added descriptions of each section

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -2,8 +2,8 @@ export default function Projects() {
     return (
     <div id ="EducationSection" className="bg-black border-white border-2 mx-30 my-30 h-100">
         <div id="EducationContainer" className="p-30">
-            <div id="EducationHeader" className="text-white m-auto text-center p-5">Education</div>
-            <div id="EducationText" className="text-white m-auto text-center">This is the text for education</div>
+            <div id="EducationHeader" className="text-white m-auto text-center p-5">Projects</div>
+            <div id="EducationText" className="text-white m-auto text-center">This is where I would show off any of my cool projects</div>
         </div>
     </div>
     );
