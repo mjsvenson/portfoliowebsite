f839a9d9d0b239b0deb5faa2706f7f375d5dde02
Svenson <msvenson@necc.mass.edu>
2025-05-09 13:43:06 -0400

    Added prototype text to aboutme section

--- src/app/Components/AboutMe.tsx
+++ src/app/Components/AboutMe.tsx
@@ -6,7 +6,13 @@ export default function AboutMe() {
         <div id="AboutMebg" className="bg-black border-white border-2 mx-30 h-100">
             <div id="AboutMeContainer" className="p-30">
                 <div id="AboutMeHeader" className="text-white m-auto text-center p-5">About me</div>
-                <div id="AboutMeText" className="text-white m-auto text-center">This is the text for the about me section</div>
+                <div id="AboutMeText" className="text-white m-auto text-center">
+                  Hello there! My name is Matthew Svenson and this is my Portfolio website! I was born in Melrose Massachusetts on April 4th 2002. 
+                  Ever since middle school I have had a passion for Computer Science, during any downtime I had in school I would mess around with HTML learning
+                  how to make basic websites and outside of school I was learning how to mod minecraft and learning how to port forward to create my own minecraft server.
+                  My passion was put into my classes when I had gone into Computing classes within my highschool where I first encountered object oriented programming 
+                  with the class being entirely in Java. I completed that class and got a 4 on my AP Computer Science exam. 
+                </div>
             </div>
         </div>
     </div>
