c9583925f2acd09e3c25f66c7aced9a43ed7e194
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2025-05-13 12:14:17 -0400

    added fractions

--- src/app/Components/Education.tsx
+++ src/app/Components/Education.tsx
@@ -2,13 +2,13 @@ export default function Education() {
     return (
     <div id ="EducationSection" className="bg-black border-white border-2 mx-30 my-30 h-100">
         <div id="title" className="text-white m-auto my-10 text-center text-[56px]"> Education </div>
-        <div id="column1" className="bg-black w-1/3 h-55 float-left">
+        <div id="column1" className="bg-black w-1/3 h-4/7 float-left">
             column1
         </div> 
-        <div id="column2" className="bg-black w-1/3 h-55 float-left">
+        <div id="column2" className="bg-black w-1/3 h-4/7 float-left">
             column2
         </div>
-        <div id="column3" className="bg-black w-1/3 h-55 float-left">
+        <div id="column3" className="bg-black w-1/3 h-4/7 float-left">
             column3
         </div>
 
