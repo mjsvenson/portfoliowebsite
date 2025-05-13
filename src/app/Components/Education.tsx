ad14bd87c47bfd93be15ffe91ce6dc4964380fe4
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2025-05-13 12:12:17 -0400

    Fixed bug with 3 columns in education section

--- src/app/Components/Education.tsx
+++ src/app/Components/Education.tsx
@@ -1,14 +1,14 @@
 export default function Education() {
     return (
-    <div id ="EducationSection" className="bg-black border-white border-2 mx-30 my-30 h-100 relative">
+    <div id ="EducationSection" className="bg-black border-white border-2 mx-30 my-30 h-100">
         <div id="title" className="text-white m-auto my-10 text-center text-[56px]"> Education </div>
-        <div id="column1" className="bg-black w-[556px] h-60 float-down bottom-0 left-0 absolute">
+        <div id="column1" className="bg-black w-1/3 h-55 float-left">
             column1
-        </div>
-        <div id="column2" className="bg-black w-[556px] left-[556px] h-60 float-down bottom-0 left-0 absolute">
+        </div> 
+        <div id="column2" className="bg-black w-1/3 h-55 float-left">
             column2
         </div>
-        <div id="column3" className="bg-black w-[556px] left-[1112px] h-60 float-down bottom-0 left-0 absolute">
+        <div id="column3" className="bg-black w-1/3 h-55 float-left">
             column3
         </div>
 
