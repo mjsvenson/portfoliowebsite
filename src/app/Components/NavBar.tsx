fe852a718c89dcd22c8f69a65ce15bc89a328fb1
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2025-05-13 12:05:05 -0400

    Added link to logo

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -5,7 +5,7 @@ import Logo from "./Images/logo.png";
 export default function NavBar() {
   return (
     <div id="NavBar" className="fixed mask-b-from-40% mask-b-to-90% bg-black w-[1910]">
-    <Image src={Logo} alt="Logo" width={100} height={100} className='float-left'/>
+    <a href='#Intro'><Image src={Logo} alt="Logo" width={100} height={100} className='float-left'/></a>
       <a href="#CertificationSection" className="text-white float-right my-[30] mx-[20]"> Certifications </a>
       <a href="#ProjectSection" className="text-white float-right my-[30] mx-[20]"> Projects </a>
       <a href="#WorkSection" className="text-white float-right my-[30] mx-[20]"> Work Experience </a>
