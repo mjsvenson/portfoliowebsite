19d43c01724120019f68e50e911aa723f0dbd656
mjsvenson <matthewjsvenson@gmail.com>
2025-05-14 15:09:32 -0400

    Fixed issue with overlaying video ontop of navbar

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -4,7 +4,7 @@ import Logo from "./Images/logo.png";
 
 export default function NavBar() {
   return (
-    <div id="NavBar" className="fixed mask-b-from-40% mask-b-to-90% bg-black w-[1910]">
+    <div id="NavBar" className="fixed mask-b-from-40% mask-b-to-90% bg-black w-[1910] z-1">
     <a href='#Intro'><Image src={Logo} alt="Logo" width={100} height={100} className='float-left'/></a>
       <a href="#CertificationSection" className="text-white float-right my-[30] mx-[20]"> Certifications </a>
       <a href="#ProjectSection" className="text-white float-right my-[30] mx-[20]"> Projects </a>
