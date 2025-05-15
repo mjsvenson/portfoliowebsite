8d1ed5bf27afa60fd5b9a7a760bdf0a84803cd1f
mjsvenson <matthewjsvenson@gmail.com>
2025-05-15 12:38:55 -0400

    Added my gpa to the education section

--- src/app/Components/Education.tsx
+++ src/app/Components/Education.tsx
@@ -8,7 +8,13 @@ export default function Education() {
         <div id="title" className="text-white m-auto my-10 text-center text-[56px]"> Education </div>
         <div id="column1" className="text-white bg-black w-1/3 h-4/7 float-left">
             <div id="imageContainer" className='px-30'><Image src={UMLLogo} alt="UML Logo here" width={200} height={100} className='float-left border-white border-2'/></div>
-            <p className='my-30 mx-10'>I graduated from University of Massachusetts, Lowell, with a degree in Computer Science and a minor in Mathematics</p>
+            <div className='my-30 mx-10'>
+                I graduated from University of Massachusetts, Lowell, with a degree in Computer Science and a minor in Mathematics
+                <br />
+                <br />
+                <div className='mx-15'>I completed my degree with a 3.4 GPA</div>
+            </div>
+            
         </div> 
         <div id="column2" className="text-white bg-black w-1/3 h-4/7 float-left">
             column2
