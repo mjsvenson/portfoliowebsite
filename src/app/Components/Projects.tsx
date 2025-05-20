02fde5499ec98b4f85bd94d74e0c3ff5d2236078
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2025-05-19 20:43:05 -0400

    Added rows to work experience section and added first work experience

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -1,8 +1,8 @@
 export default function Projects() {
     return (
     <div id ="ProjectSection" className="bg-black border-white border-2 mx-30 my-30 h-100">
+                    <div id="EducationHeader" className="text-white m-auto text-center p-5">Projects</div>
         <div id="EducationContainer" className="p-30">
-            <div id="EducationHeader" className="text-white m-auto text-center p-5">Projects</div>
             <div id="EducationText" className="text-white m-auto text-center">This is where I would show off any of my cool projects</div>
         </div>
     </div>
--- src/app/Components/WorkExperience.tsx
+++ src/app/Components/WorkExperience.tsx
@@ -1,9 +1,54 @@
+import Image from "next/image";
+import NECCLogo from "./Images/necc-logo-sq.png"
 export default function WorkExperience() {
     return (
-    <div id ="WorkSection" className="bg-black border-white border-2 mx-30 my-30 h-100">
-        <div id="EducationContainer" className="p-30">
-            <div id="EducationHeader" className="text-white m-auto text-center p-5">Work Experience</div>
-            <div id="EducationText" className="text-white m-auto text-center">This is where I would speak on my work experience</div>
+    <div id ="WorkSection" className="bg-black border-white border-2 mx-30 my-30 h-200">
+        
+        <div id="WorkExperienceHeader" className="text-white m-auto text-center my-10 text-[56px]">Work Experience</div>
+        
+        <div id="WorkExperienceContainerNECC" className="mx-20 mb-10 float-left">
+            <Image src={NECCLogo} alt="NECC Logo Here" width={150} className="float-left"/>
+            <div id="WorkTitle" className="text-white float-left mx-10 text-[28px]">Software Engineer Intern</div>
+            <div id="WorkResponsibilities" className="text-white float-left w-1/3">
+            <ul className="list-inside list-disc">
+                <li>In this position, I am engineering a format-dynamic employee information document using JavaScript to validate field entries and
+                SQL to autofill participant information accurately.</li>
+                <br />
+                <li>I am also collaborating with cross-functional teams within the college to design, review, and refine document layouts,
+                ensuring alignment with project goals.</li>
+            </ul>
+            </div>
+            <div id="WorkLength" className="text-white float-left mx-20 text-[32px]">November 2024 - Present</div>
+        </div>
+
+        <div id="WorkExperienceContainerNECC" className="mx-20 mb-10 float-left">
+            <Image src={NECCLogo} alt="NECC Logo Here" width={150} className="float-left"/>
+            <div id="WorkTitle" className="text-white float-left mx-10 text-[28px]">Software Engineer Intern</div>
+            <div id="WorkResponsibilities" className="text-white float-left w-1/3">
+            <ul className="list-inside list-disc">
+                <li>In this position, I am engineering a format-dynamic employee information document using JavaScript to validate field entries and
+                SQL to autofill participant information accurately.</li>
+                <br />
+                <li>I am also collaborating with cross-functional teams within the college to design, review, and refine document layouts,
+                ensuring alignment with project goals.</li>
+            </ul>
+            </div>
+            <div id="WorkLength" className="text-white float-left mx-20 text-[32px]">November 2024 - Present</div>
+        </div>
+
+        <div id="WorkExperienceContainerNECC" className="mx-20 mb-10 float-left">
+<Image src={NECCLogo} alt="NECC Logo Here" width={150} className="float-left"/>
+            <div id="WorkTitle" className="text-white float-left mx-10 text-[28px]">Software Engineer Intern</div>
+            <div id="WorkResponsibilities" className="text-white float-left w-1/3">
+            <ul className="list-inside list-disc">
+                <li>In this position, I am engineering a format-dynamic employee information document using JavaScript to validate field entries and
+                SQL to autofill participant information accurately.</li>
+                <br />
+                <li>I am also collaborating with cross-functional teams within the college to design, review, and refine document layouts,
+                ensuring alignment with project goals.</li>
+            </ul>
+            </div>
+            <div id="WorkLength" className="text-white float-left mx-20 text-[32px]">November 2024 - Present</div>
         </div>
     </div>
     );
