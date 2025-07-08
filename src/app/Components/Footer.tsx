694d149f5e287bee532924e8295d0bd9dc875b05
Matthew Svenson <71236924+mjsvenson@users.noreply.github.com>
2025-07-08 13:38:35 -0400

    Started on the footer, almost ready for prod

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
     <div id ="Footer">
-        
+        <div id="FooterContainer" className="w-full text-center"> 
+            Matthew Svenson Production
+        </div>
     </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
       <Education />
       <WorkExperience />
       <Projects />
-      <Certifications />
       <Footer />
      </div>
   );
