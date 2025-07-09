16200046978bcb88b607b95083a931c3e69fdab0
mjsvenson <matthewjsvenson@gmail.com>
2025-07-09 14:43:29 -0400

    Finished footer, ready for prod

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,8 +1,15 @@
 export default function Footer() {
     return (
     <div id ="Footer">
-        <div id="FooterContainer" className="w-full text-center"> 
-            Matthew Svenson Production
+        <div id="FooterContainer" className="relative w-full text-center text-white p-5"> 
+            <div id="leftSide" className="bottom-0 left-0 text-left w-1/4">
+                Matthew Svenson <br/>
+                978-854-3352
+            </div>
+            <div id="rightSide" className="absolute bottom-0 right-0 text-right w-1/4 p-5">
+                matthewjsvenson@gmail.com <br/>
+                <a href="https://www.linkedin.com/in/matthew-svenson/" className="text-sky-500">Linkedin</a>
+            </div>
         </div>
     </div>
     );
--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -6,7 +6,6 @@ export default function NavBar() {
   return (
     <div id="NavBar" className="fixed mask-b-from-40% mask-b-to-90% bg-black w-[1910] z-1">
     <a href='#Intro'><Image src={Logo} alt="Logo" width={100} height={100} className='float-left'/></a>
-      <a href="#CertificationSection" className="text-white float-right my-[30] mx-[20]"> Certifications </a>
       <a href="#ProjectSection" className="text-white float-right my-[30] mx-[20]"> Projects </a>
       <a href="#WorkSection" className="text-white float-right my-[30] mx-[20]"> Work Experience </a>
       <a href="#EducationSection" className="text-white float-right my-[30] mx-[20]"> Education </a>
